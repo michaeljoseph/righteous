b634b54fbc126ba0f40a3e01e74263cf0ddf8f0e
Michael Joseph <michaeljoseph+github@gmail.com>
2012-07-04 18:55:55 +0200

    Specify the imported api methods and update the version

--- righteous/__init__.py
+++ righteous/__init__.py
@@ -1,4 +1,9 @@
-__version__ = '0.0.1'
+__version__ = '0.1.1'
 __author__ = 'Michael Joseph'
 
-from api import *
+from .api import (
+    init, login,
+    list_servers, find_server, server_info, server_settings,
+    create_and_start_server, create_server, set_server_parameters,
+    start_server, stop_server, delete_server,
+)
