4dcb257c6f7f1189fdd427066828127e821f37af
Michael Joseph <michaeljoseph+github@gmail.com>
2012-07-06 08:39:25 +0200

    Silence pyflakes unused import warning

--- righteous/__init__.py
+++ righteous/__init__.py
@@ -7,3 +7,7 @@ from .api import (
     create_and_start_server, create_server, set_server_parameters,
     start_server, stop_server, delete_server,
 )
+# defeat pyflakes
+init; login; list_servers; find_server; server_info; server_settings
+create_and_start_server; create_server; set_server_parameters
+start_server; stop_server; delete_server
