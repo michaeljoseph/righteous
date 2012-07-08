79b99e87e555d290a44573029952175be054a359
Michael Joseph <michaeljoseph+github@gmail.com>
2012-07-08 23:18:34 +0200

    Clearer transient variable name

--- righteous/__init__.py
+++ righteous/__init__.py
@@ -7,8 +7,8 @@ from .api import (
     create_and_start_server, create_server, set_server_parameters,
     start_server, stop_server, delete_server,
 )
-# defeat pyflakes
-py_flakes = (init, login, list_servers, find_server, server_info,
+
+hush_pyflakes = (init, login, list_servers, find_server, server_info,
     server_settings, create_and_start_server, create_server,
     set_server_parameters, start_server, stop_server, delete_server
 )
