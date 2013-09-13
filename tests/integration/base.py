4af0a8584b39f450a00cb813e73266d7f55a66d8
Michael Joseph <michaeljoseph+github@gmail.com>
2013-09-14 01:15:36 +0200

    Integration test for login

--- tests/integration/base.py
+++ tests/integration/base.py
@@ -1,12 +1,10 @@
-from testify import (TestCase, class_setup, setup)
 import righteous
 from ConfigParser import SafeConfigParser
+from ..compat import unittest
 
+class RighteousIntegrationTestCase(unittest.TestCase):
 
-class RighteousIntegrationTestCase(TestCase):
-
-    @class_setup
-    def initialise_righteous(self):
+    def setUp(self):
         config = SafeConfigParser()
         config.read('righteous.config')
         if not config.has_section('auth'):
@@ -24,10 +22,11 @@ class RighteousIntegrationTestCase(TestCase):
             self.auth['username'], self.auth['password'],
             self.auth['account_id'], **self.server)
 
-        if not righteous.config.settings.cookies:
-            righteous.login()
-        self.config = config
 
-    @setup
-    def prepare_test(self):
+        self.config = config
         self.username = self.auth['username']
+
+
+    def test_login(self):
+        self.assertTrue(righteous.login())
+        assert False
